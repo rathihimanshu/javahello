300e3b45e953183aec663ca897cf9412ed2eee42
Himanshu <rathihimanshu@live.com>
2021-04-03 20:59:54 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
 	
+package com.helloall.main;
+
 public class Hello{
 		
 	
-		private Hello() { }
+		//private Hello() { }
 		public static void main(String[] args) {
 			for(int i=0;i<10;i++) {
 				System.out.println("Hello..."+i);
