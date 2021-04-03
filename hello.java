6428061652346c6d742ecb7c73812e25aff6d6f5
Himanshu <rathihimanshu@live.com>
2021-04-03 21:02:10 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -7,7 +7,8 @@ public class Hello{
 		//private Hello() { }
 		public static void main(String[] args) {
 			for(int i=0;i<10;i++) {
-				System.out.println("Hello..."+i);
+			//	System.out.println("Hello..."+i);
+				LOGGER.log("Hello"+i);
 			}
 		}
 }
